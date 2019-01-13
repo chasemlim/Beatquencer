f402c1dd5a0b3514491a86d418b0525d2279a672
chasemlim <chasemlim@gmail.com>
2019-01-13 14:11:33 -0800

    Add new audio assets and begin styling for main board

--- src/lib/pad.js
+++ src/lib/pad.js
@@ -35,23 +35,25 @@ export default class Pad {
     determineSound() {
         switch (this.soundType) {
             case 'hihat':
-                // this.sound = 
+                return '../src/assets/audio/hihat.wav';
             case 'tom1':
-
+                return '../src/assets/audio/tom1.wav';
             case 'tom2':
-
+                return '../src/assets/audio/tom2.wav';
             case 'tom3':
-
+                return '../src/assets/audio/tom3.wav';
             case 'tom4':
-
-            case 'snare':
-
+                return '../src/assets/audio/tom4.wav';
+            case 'snare1':
+                return '../src/assets/audio/snare1.wav';
+            case 'snare2':
+                return '../src/assets/audio/snare1.wav';
             case 'clap':
-
+                return '../src/assets/audio/clap.wav';
             case 'kick':
-
+                return '../src/assets/audio/kick.wav';
             case 'sub':
-
+                return '../src/assets/audio/subbass.wav';
             default:
                 return '../src/assets/audio/drums.wav';
         }
