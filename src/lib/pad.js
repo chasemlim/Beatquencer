0d62ab49bc56a20ed8351f1a2a3eb9c30a4c2e02
chasemlim <chasemlim@gmail.com>
2019-01-18 11:07:50 -0800

    Testing for github deploy

--- src/lib/pad.js
+++ src/lib/pad.js
@@ -14,27 +14,27 @@ export default class Pad {
     determineSound() {
         switch (this.soundType) {
             case 'hihat':
-                return './src/assets/audio/hihat.wav';
+                return '/src/assets/audio/hihat.wav';
             case 'tom1':
-                return './src/assets/audio/tom1.wav';
+                return '/src/assets/audio/tom1.wav';
             case 'tom2':
-                return './src/assets/audio/tom2.wav';
+                return '/src/assets/audio/tom2.wav';
             case 'tom3':
-                return './src/assets/audio/tom3.wav';
+                return '/src/assets/audio/tom3.wav';
             case 'tom4':
-                return './src/assets/audio/tom4.wav';
+                return '/src/assets/audio/tom4.wav';
             case 'snare1':
-                return './src/assets/audio/snare1.wav';
+                return '/src/assets/audio/snare1.wav';
             case 'snare2':
-                return './src/assets/audio/snare2.wav';
+                return '/src/assets/audio/snare2.wav';
             case 'clap':
-                return './src/assets/audio/clap.wav';
+                return '/src/assets/audio/clap.wav';
             case 'kick':
-                return './src/assets/audio/kick.wav';
+                return '/src/assets/audio/kick.wav';
             case 'sub':
-                return './src/assets/audio/subbass.wav';
+                return '/src/assets/audio/subbass.wav';
             default:
-                return './src/assets/audio/drums.wav';
+                return '/src/assets/audio/drums.wav';
         }
     } //
 
