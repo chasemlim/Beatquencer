ad28b5514159656f219bc0433a69400b8e838dbf
chasemlim <chasemlim@gmail.com>
2019-01-30 10:50:45 -0800

    Remove logs and add CTA

--- src/lib/playpause.js
+++ src/lib/playpause.js
@@ -2,15 +2,6 @@ export default class PlayButton {
 
     constructor(audioCtx) {
         this.audioCtx = audioCtx;
-        // const playPause = document.querySelector('playpause');
-
-        // playPause.addEventListener('click', () => {
-
-        //     if (playPause.hasAttribute('data-playing')) {
-        //         console.log("YES");
-
-        //     }
-        // }) This might have to go in index.js
     }
 
 }
